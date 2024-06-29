cc3fc35e3a1edb913f77c8068c4dfa3d5fc77393
Sneha Motghare <snehamotghare1127@gmail.com>
2024-06-29 18:54:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+
+      <a href="container1"><button id="a"> Step1</button> </a>
+       <a href ="#d"><button id="b"> Step2</button></a> 
+       <a href ="#f">  <button id="c"> Step3</button></a>
+
+      
+                <div class="container1"  >
+
+                  <h1 id='S1'>Step1</h1>
+
+
+        </div>
+         
+        <img id='d' href="#" src="vector1.png" />
+      
+
+        <div class="container2"  >
+
+        <h1 id='S2'>Step2</h1>
+        </div>
+
+        <img id='e' href="#" src="vector3.png" />
+    
+        <div class="container3"  >
+        <h1 id='S3'>Step3</h1>
+
+       </div>
+        
+        <img id='f' href="#" src="vector2.png" />
+        
+    
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </body>
+
+
+  );
+}
+
+export default App;
